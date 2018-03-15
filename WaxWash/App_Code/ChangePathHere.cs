6c34548c8feb8e4ae0497a4b3e1bf9900e7c217d
Bogdan <munteanbogdan97@gmail.com>
2018-03-15 12:31:55 -0400

    somethig

--- WaxWash/App_Code/ChangePathHere.cs
+++ WaxWash/App_Code/ChangePathHere.cs
@@ -9,7 +9,7 @@ public class ChangePathHere
     
  
     static string mantas_PATH = @"Data Source=.\sqlexpress;Initial Catalog=C:\USERS\MANTA\DESKTOP\WAXWASH\WAXWASH\APP_DATA\WAXWASH2.0.MDF;Integrated Security=True";
-    static string bog_PATH = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Bogdan\Desktop\wax\WaxWash\wax.mdf;Integrated Security = True; Connect Timeout = 30";
+    static string bog_PATH = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Bogdan\Desktop\waxwasz\WaxWash\wax.mdf;Integrated Security = True; Connect Timeout = 30";
     static string matt_PATH = @"//add your path here";
 
 
