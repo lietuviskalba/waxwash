589ea7d4e0624798e5f2a642bb6c884e328117d2
LietuvisKalba <mantasling@gmail.com>
2018-03-18 11:54:55 -0400

    Polished up the login, register, login confirm and feedback confirm page

---
+++ WaxWash/App_Code/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Users
+{
+    public string userId;
+    public string f_name;
+    public string l_name;
+    public string address;
+    public string state;
+    public string email;
+    public string username;
+    public string passwprd;
+
+    public Users()
+    {
+    }
+}
