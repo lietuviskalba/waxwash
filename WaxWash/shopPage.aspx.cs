495c7383c53e66ca4f54d1683ce0cb7372f8db89
Bogdan <munteanbogdan97@gmail.com>
2018-04-20 12:37:29 -0400

    something

--- WaxWash/shopPage.aspx.cs
+++ WaxWash/shopPage.aspx.cs
@@ -87,13 +87,4 @@ public partial class shopPage : System.Web.UI.Page
     {
         Server.Transfer("shoppingPage.aspx");
     }
-
-<<<<<<< HEAD
-=======
-    public void Reload() {
-
-        var id = ddlWashType.SelectedValue;
-        //var url = Friendly;
-    }
->>>>>>> 182e62bb805635ac7cdb5a781342e52cb107df43
 }
